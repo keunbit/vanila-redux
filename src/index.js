eb6b23a54b4b10f284f188607cd551252c438af2
keunbit <eoghks319@gmail.com>
2023-01-28 20:26:35 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
 import {createStore} from 'redux';
 
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
 	if (action.type === 'ADD') {
 		return count + 1;
 	} else if (action.type === 'MINUS') {
+		console.log('click minus');
 		return count - 1;
 	} else {
 		return count;
 	}
 };
-
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'MINUS'});
-countStore.dispatch({type: 'MINUS'});
-countStore.dispatch({type: 'MINUS'});
-countStore.dispatch({type: 'MINUS'});
+const onChange = () => {
+	number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+	countStore.dispatch({type: 'ADD'});
+};
+
+const handleMinus = () => {
+	countStore.dispatch({type: 'MINUS'});
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
