d332c409a57cda2e4c99cb3dd7b785e021c0d63e
keunbit <eoghks319@gmail.com>
2023-01-29 15:24:08 +0900

    Connecting the Store

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import {createStore} from 'redux';
+
+const ADD = 'ADD';
+const DELETE = 'DELETE';
+
+const addTodo = (text) => {
+	return {
+		type: ADD,
+		text,
+	};
+};
+
+const reducer = (state = [], action) => {
+	switch (action.type) {
+		case ADD:
+			return [{text: action.text, id: Date.now()}, ...state];
+		case DELETE:
+			return state.filter((toDo) => toDo.id !== action.id);
+		default:
+			return state;
+	}
+};
+const store = createStore(reducer);
+
+export default store;
