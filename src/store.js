b5f466dd7d69648fe40515089492d642f9810e63
keunbit <eoghks319@gmail.com>
2023-02-13 14:56:56 +0900

    createAction & createReducer

--- src/store.js
+++ src/store.js
@@ -1,32 +1,28 @@
 import {createStore} from 'redux';
+import {createAction, createReducer} from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
+const addTodo = createAction('ADD');
+const deleteTodo = createAction('DELETE');
 
-const addTodo = (text) => {
-	return {
-		type: ADD,
-		text,
-	};
-};
+// const reducer = (state = [], action) => {
+// 	switch (action.type) {
+// 		case addTodo.type:
+// 			return [{text: action.payload, id: Date.now()}, ...state];
+// 		case deleteTodo.type:
+// 			return state.filter((toDo) => toDo.id !== action.payload);
+// 		default:
+// 			return state;
+// 	}
+// };
 
-const deleteTodo = (id) => {
-	return {
-		type: DELETE,
-		id: parseInt(id),
-	};
-};
+const reducer = createReducer([], {
+	[addTodo]: (state, action) => {
+		state.push({text: action.payload, id: Date.now()});
+	},
+	[deleteTodo]: (state, action) =>
+		state.filter((toDo) => toDo.id !== action.payload),
+});
 
-const reducer = (state = [], action) => {
-	switch (action.type) {
-		case ADD:
-			return [{text: action.text, id: Date.now()}, ...state];
-		case DELETE:
-			return state.filter((toDo) => toDo.id !== action.id);
-		default:
-			return state;
-	}
-};
 const store = createStore(reducer);
 
 export const actionCreators = {
